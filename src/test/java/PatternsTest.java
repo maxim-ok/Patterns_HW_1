490712d6fa1bac85c0e9d0c11a542df2f3ec9af6
Maksim Okunev <max.vic.okunev@gmail.com>
2022-11-20 23:59:30 +0300

    add rebooking test

--- src/test/java/PatternsTest.java
+++ src/test/java/PatternsTest.java
@@ -3,11 +3,13 @@ import data.ClientInfo;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import utils.DataGenerator;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PatternsTest {
@@ -31,11 +33,43 @@ public class PatternsTest {
         $x("//span[@data-test-id='name']//input").val(clientInfo.getName());
         $x("//span[@data-test-id='phone']//input").val(clientInfo.getPhone());
         $x("//label[@data-test-id='agreement']").click();
-        $x("//*[contains(text(), 'Забронировать')]").click();
-        $x("//div[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
-        $(".notification__content")
-                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
-                .shouldBe(Condition.visible);
+        $x("//*[contains(text(), 'Запланировать')]").click();
+        $(".notification__title").shouldHave(text("Успешно!"));
+        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + planningDate));
+
+    }
+
+
+    @Test
+    void rebookingDate() {
+
+        String planningDate = generateDate(3);
+        String rebookingDate = generateDate(4);
+
+        ClientInfo clientInfo = DataGenerator.InfoFilling.clientInfo("ru");
+
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue(clientInfo.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue(clientInfo.getName());
+        $("[data-test-id=phone] input").setValue(clientInfo.getPhone());
+        $("[data-test-id=agreement] span").click();
+        $x("//*[contains(text(), 'Запланировать')]").click();
+        $("[data-test-id=success-notification]").shouldBe(visible);
+        $(".notification__title").shouldHave(text("Успешно!"));
+        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + planningDate));
+        $("[data-test-id=success-notification] button").click();
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(rebookingDate);
+        $x("//*[contains(text(), 'Запланировать')]").click();
+        $("[data-test-id=replan-notification]").shouldBe(visible);
+        $$(".notification__title").findBy(text("Необходимо подтверждение")).should(exist);
+        $$(".notification__content").findBy(text("У вас уже запланирована встреча на другую дату. Перепланировать?")).should(exist);
+        $("[data-test-id=replan-notification] button").click();
+        $("[data-test-id=success-notification]").shouldBe(visible);
+        $(".notification__title").shouldHave(text("Успешно!"));
+        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + rebookingDate));
 
     }
 
